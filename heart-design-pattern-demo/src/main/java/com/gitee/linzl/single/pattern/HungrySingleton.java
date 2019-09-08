680159389eda36ab578f1f415b3054062d19a9dd
linzl <2225010489@qq.com>
2019-09-08 23:52:09 +0800

    添加注释

--- heart-design-pattern-demo/src/main/java/com/gitee/linzl/single/pattern/HungrySingleton.java
+++ heart-design-pattern-demo/src/main/java/com/gitee/linzl/single/pattern/HungrySingleton.java
@@ -1,14 +1,14 @@
 package com.gitee.linzl.single.pattern;
 
 /**
- * 饿汉模式：肚子饿嘛，要提前吃
+ * 饿汉模式：肚子饿嘛，要提前准备好吃的
  * 
  * @author GDCC i心灵鸡汤you email:2225010489@qq.com 2013-4-27 下午08:38:31
  */
 
 public class HungrySingleton {
 
-	private static HungrySingleton single = new HungrySingleton();
+	private static final HungrySingleton single = new HungrySingleton();
 
 	private HungrySingleton() {
 	}
--- heart-design-pattern-demo/src/main/java/com/gitee/linzl/single/pattern/LazySingleton.java
+++ heart-design-pattern-demo/src/main/java/com/gitee/linzl/single/pattern/LazySingleton.java
@@ -1,11 +1,10 @@
 package com.gitee.linzl.single.pattern;
 
 /**
- * 懒汉模式：因为懒，所以等到要用的声明才初始化，但是人家正在用的话(synchronized)，你就得等咯
+ * 懒汉模式：因为懒，所以等到要用的声明才准备吃东西(初始化)，但是人家正在用的话(synchronized)，你就得等咯
  * 
  * @author GDCC i心灵鸡汤you email:2225010489@qq.com 2013-4-27 下午08:38:31
  */
-
 public class LazySingleton {
 	private volatile static LazySingleton single;
 

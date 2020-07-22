c5fbba26cbbfb06f06fdee7163b69125d4800838
linzhenlie <linzhenlie-jk@360jinrong.net>
2020-07-22 15:05:47 +0800

    refactor

--- heart-base-util/src/main/java/com/gitee/linzl/cipher/asymmetrical/CipherSignature.java
+++ heart-base-util/src/main/java/com/gitee/linzl/cipher/asymmetrical/CipherSignature.java
@@ -1,14 +1,9 @@
 package com.gitee.linzl.cipher.asymmetrical;
 
-import java.util.ArrayList;
-import java.util.Base64;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-
 import com.gitee.linzl.cipher.asymmetrical.AsymmetricCipherBuilder.EncryptVerifyBuilder;
 import com.gitee.linzl.lang.StringUtil;
+
+import java.util.*;
 
 public class CipherSignature {
 	/**
@@ -20,15 +15,15 @@ public class CipherSignature {
 		List<String> keys = new ArrayList<>(sortedParams.keySet());
 		Collections.sort(keys);
 		StringBuffer content = new StringBuffer();
-		int index = 0;
 		for (int i = 0,size = keys.size(); i < size; i++) {
 			String key = keys.get(i);
 			String value = sortedParams.get(key);
 			if (StringUtil.isEmpty(key, value)) {
-				content.append((index == 0 ? "" : "&") + key + "=" + value);
-				index++;
+				content.append(key).append("=").append(value).append("&");
 			}
 		}
+		// 删除最后一个&
+		content.delete(content.length() - 1, content.length());
 		return content.toString();
 	}
 
